d81f3467c6d3c79726e8c4624a890e8296fdef18
AndrewSkelly <andrewskelly@hotmail.co.uk>
2024-11-09 18:45:17 +0000

    Removed / from the date string as was invalid

--- tests/FirstTest.spec.ts
+++ tests/FirstTest.spec.ts
@@ -3,15 +3,21 @@ import { test, expect } from '@playwright/test';
 test('Add a TODO item with the text: TODO 1', async ({ page }) => {
   await page.goto('https://todomvc.com/examples/react/dist/');
 
+  // Create a new date object then set currentDate = a date string
+  // Found on W3S https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_tolocaledatestring
   const date = new Date();
   let currentDate = date.toLocaleDateString();
+  // Issue with this as the forward slash causes random symbols and must not be valid so I removed it
+  let formattedCurrentDate = currentDate.split('/');
 
-  // Find Input area by placeholder shown and fill with Todo 1
-  await page.getByPlaceholder('What needs to be done?').fill('TODO 1 - ' + currentDate);
+  // Find Input area by ID and fill with Todo 1 & date
+  await page.fill('#todo-input', `TODO 1 - ${formattedCurrentDate}`);
+
+  // Had to be used because there was no submit button
   await page.keyboard.press('Enter');
   
   // Expects page to have a heading with the name of Installation.
-  await expect(page.getByLabel('TODO 1 - ' + currentDate)).toBeVisible();
+  await expect(page.getByLabel('TODO 1 - ' + formattedCurrentDate)).toBeVisible();
 });
 
 // test('get started link', async ({ page }) => {
