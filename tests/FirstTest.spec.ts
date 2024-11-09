40dcad540e38f8d41bd42469613211bd6cdb3e4d
AndrewSkelly <andrewskelly@hotmail.co.uk>
2024-11-09 22:12:23 +0000

    Realised it is one test combined both into one, checked box and verified using css locator

--- tests/FirstTest.spec.ts
+++ tests/FirstTest.spec.ts
@@ -1,32 +1,64 @@
 import { test, expect } from '@playwright/test';
 
-test('Add a TODO item with the text: TODO 1', async ({ page }) => {
+
+// set Today and Tomorrows Date
+// https://dev.to/falselight/how-to-get-tomorrows-date-in-javascript-a-beginners-guide-3p7e
+let currentDate = new Date();
+let tomorrow = new Date(currentDate);
+tomorrow.setDate(tomorrow.getDate() + 1);
+
+// Convert to a Formatted string and remove the /
+let currentDateFormatted = currentDate.toLocaleDateString();
+let tomorrowDateFormatted = tomorrow.toLocaleDateString();
+let currentDateFormattedString = currentDateFormatted.split('/');
+let TomorrowDateFormattedString = tomorrowDateFormatted.split('/');
+
+// Verify Dates are correct for myself
+console.log(currentDateFormattedString);
+console.log(TomorrowDateFormattedString);
+
+// ### Depricated: Create a new date object then set currentDate = a date string
+// Found on W3S https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_tolocaledatestring
+// const date = new Date();
+// let currentDate = date.toLocaleDateString();
+// Issue with this as the forward slash causes random symbols and must not be valid so I removed it
+// let formattedCurrentDate = currentDate.split('/');
+
+test('Add a TODO item with the text “TODO 1 - ” concatenated with the current date.', async ({ page }) => {
   await page.goto('https://todomvc.com/examples/react/dist/');
 
-  // Create a new date object then set currentDate = a date string
-  // Found on W3S https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_tolocaledatestring
-  const date = new Date();
-  let currentDate = date.toLocaleDateString();
-  // Issue with this as the forward slash causes random symbols and must not be valid so I removed it
-  let formattedCurrentDate = currentDate.split('/');
-
   // Find Input area by ID and fill with Todo 1 & date
-  await page.fill('#todo-input', `TODO 1 - ${formattedCurrentDate}`);
+  await page.fill('#todo-input', `TODO 1 - ${currentDateFormattedString}`);
 
   // Had to be used because there was no submit button
   await page.keyboard.press('Enter');
-  
+
+  // Find Input area by ID and fill with Todo 1 & date
+  await page.fill('#todo-input', `TODO 2 - ${TomorrowDateFormattedString}`);
+
+  // Had to be used because there was no submit button
+  await page.keyboard.press('Enter');
+
+  //https://playwright.dev/docs/locators#locate-by-text
+  // Had to use CSS as both todo items were identical including ID.
+  await page.locator('css=.todo-list > li:nth-child(1) > div:nth-child(1) > input:nth-child(1)').check();
+
+  // Check Checkbox by using label
+  //https://playwright.dev/docs/input
+
   // Expects page to have text including below
   // Couldnt make getByLabel to work so used text instead as a verification the object is created
-  await expect(page.getByText(`TODO 1 - ${formattedCurrentDate}`)).toBeVisible();
+  await expect(page.getByText(`TODO 1 - ${currentDateFormattedString}`)).toBeVisible();
+
+  await expect(page.getByText(`TODO 2 - ${TomorrowDateFormattedString}`)).toBeVisible();
+
+  await expect(page.locator('css=.todo-list > li:nth-child(1) > div:nth-child(1) > input:nth-child(1)')).toBeChecked();
 });
 
-// test('get started link', async ({ page }) => {
+// test('Add a TODO item with the text “TODO 2 - ” concatenated with the next day.', async ({ page }) => {
 //   await page.goto('https://todomvc.com/examples/react/dist/');
 
-//   // Click the get started link.
-//   await page.getByRole('link', { name: 'Get started' }).click();
-
-//   // Expects page to have a heading with the name of Installation.
-//   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+//   // Expects page to have text including below
+//   // Couldnt make getByLabel to work so used text instead as a verification the object is created
+//   await expect(page.getByText(`TODO 2 - ${TomorrowDateFormattedString}`)).toBeVisible();
 // });
